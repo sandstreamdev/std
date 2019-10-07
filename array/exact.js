89488a44a089ec387da35b606c43268cbd775366
Przemysław Zalewski <przemyslaw.zalewski@sandstream.pl>
2019-10-07 18:39:40 +0200

    Improve adjust exact and range implementation

--- array/exact.js
+++ array/exact.js
@@ -1,4 +1,3 @@
-export default n => source =>
-  Array(n)
-    .fill()
-    .map((_, i) => source[i]);
+import range from "./range.js";
+
+export default n => xs => range(n).map(index => xs[index]);
