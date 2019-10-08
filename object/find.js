43956f2c5b250daadb9838b8580706bede62b3a4
Przemysław Zalewski <przemyslaw.zalewski@sandstream.pl>
2019-10-08 09:54:12 +0200

    fix: return a value from object find, not an entry

--- object/find.js
+++ object/find.js
@@ -1,4 +1,8 @@
 import entries from "./entries.js";
 
-export default predicate => xs =>
-  entries(xs).find(([key, value]) => predicate(value, key, xs));
+export default predicate => xs => {
+  const [, value] =
+    entries(xs).find(([key, value]) => predicate(value, key, xs)) || [];
+
+  return value;
+};
