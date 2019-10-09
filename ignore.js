23fbb1cb2b6de156616a0766afaeffc0862281a9
Przemysław Zalewski <przemyslaw.zalewski@sandstream.pl>
2019-10-09 20:02:46 +0200

    Define list of ignored files and directories

---
+++ ignore.js
@@ -0,0 +1,21 @@
+const ignoredFiles = [
+  ".all-contributorsrc",
+  ".eslintrc.js",
+  ".gitignore",
+  ".npmignore",
+  "compile.js",
+  "index.cjs.js",
+  "index.js",
+  "index.test.js",
+  "index.umd.js",
+  "LICENSE",
+  "package-lock.json",
+  "package.json",
+  "README.md",
+  "regenerate.js",
+  "rollup.config.js"
+];
+
+const ignoredDirectories = [".git", ".github", "node_modules"];
+
+export default [ignoredFiles, ignoredDirectories];
