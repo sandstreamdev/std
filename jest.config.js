2c9f514f1d6ba245873100461170a3ff0f8ca949
Przemysław Zalewski <przemyslaw.zalewski@sandstream.pl>
2019-10-16 21:31:46 +0200

    Collect coverage from .js files for now

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,24 @@
 /* eslint-env commonjs */
 
+const ignoredFiles = [
+  "addExtensions\\.js$",
+  "compile\\.js$",
+  "ignore\\.js$",
+  "index\\.cjs\\.js$",
+  "index\\.umd\\.js$",
+  "jest\\.config\\.js$",
+  "regenerate\\.js$",
+  "rollup\\.config\\.js$"
+];
+
 module.exports = {
   collectCoverage: true,
-  collectCoverageFrom: ["**/*.ts"],
-  coveragePathIgnorePatterns: ["\\.d\\.ts$", "index\\.ts"],
+  collectCoverageFrom: ["**/*.js"],
+  coveragePathIgnorePatterns: [
+    "\\.d\\.ts$",
+    "index\\.ts",
+    "index\\.js",
+    ...ignoredFiles
+  ],
   coverageDirectory: ".coverage"
 };
