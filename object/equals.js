e7dc9c6a3d26977beea0c65c437dc3d56da07c2f
Pawel Stanecki <pawel.stanecki@sandstream.pl>
2019-10-25 14:53:59 +0200

    Commit after build

--- object/equals.js
+++ object/equals.js
@@ -1,6 +1,6 @@
-import isObject from '../is/object.js';
-import areArrays from '../array/are.js';
-import lengthDiffers from '../array/lengthDiffers.js';
+import isObject from "../is/object.js";
+import areArrays from "../array/are.js";
+import lengthDiffers from "../array/lengthDiffers.js";
 
 const keySet = (a, b) => [...new Set([...Object.keys(a), ...Object.keys(b)])];
 
