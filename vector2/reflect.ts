88551a8f0a734f69c769efa03c1522d22849d9ac
Przemysław Zalewski <przemyslaw.zalewski@sandstream.pl>
2019-12-05 09:58:47 +0100

    Regenerate code

--- vector2/reflect.ts
+++ vector2/reflect.ts
@@ -1,4 +1,8 @@
 import sub from "./sub";
 import dot from "./dot";
 
-export default (a, v) => sub(v, a.map(_ => (_ * 2 * dot(v, a)) / dot(a, a)));
+export default (a, v) =>
+  sub(
+    v,
+    a.map(_ => (_ * 2 * dot(v, a)) / dot(a, a))
+  );
