8718a140eb426f2f030dab78f4a1fd9732ab0a95
Przemysław Zalewski <przemyslaw.zalewski@sandstream.pl>
2019-12-05 10:28:07 +0100

    Improve standard deviation tests

--- math/standardDeviation.test.ts
+++ math/standardDeviation.test.ts
@@ -2,8 +2,14 @@
 // @ts-ignore ambiguous import
 import standardDeviation from "./standardDeviation.ts";
 
-describe("average", () => {
+describe("standardDeviation", () => {
   it("returns 0 for a list of equal elements", () => {
     expect(standardDeviation([5, 5, 5, 5, 5, 5])).toBe(0);
+  });
+
+  it("should calculate standard deviation of given data", () => {
+    expect(standardDeviation([20, 31, 83, 73, 89, 90, 30, 79, 44, 73])).toBe(
+      Math.sqrt(32758 / 5) / 3
+    );
   });
 });
