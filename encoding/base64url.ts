eeaea0b255e06d6550dd2395a2063550b7824f8b
Przemysław Zalewski <przemyslaw.zalewski@sandstream.pl>
2019-12-12 16:10:38 +0100

    Refactor base64url module

--- encoding/base64url.ts
+++ encoding/base64url.ts
@@ -1,15 +1,48 @@
 /* eslint-env browser, node */
 
-export const encode = _ =>
-  btoa(_)
+export const toByteString = bytes =>
+  bytes.map(_ => String.fromCharCode(_)).join("");
+
+export const fromByteString = (byteString: string) =>
+  [...byteString].map(_ => _.codePointAt(0));
+
+const ENCODING = "utf-8";
+
+const btoaImplementation = (text: string) =>
+  typeof window !== "undefined"
+    ? btoa(toByteString([...new window.TextEncoder().encode(text)]))
+    : Buffer.from(text, ENCODING).toString("base64");
+
+const atobImplementation = (text: string) =>
+  typeof window !== "undefined"
+    ? new window.TextDecoder(ENCODING).decode(
+        new Uint8Array(fromByteString(atob(text)))
+      )
+    : Buffer.from(text, "base64").toString(ENCODING);
+
+export const encodeString = (text: string) =>
+  btoaImplementation(text)
     .replace(/=/g, "")
     .replace(/\+/g, "-")
     .replace(/\//g, "_");
 
-export const toBase64Url = base64 =>
+export const decodeString = (text: string) =>
+  atobImplementation(text.replace(/\-/g, "+").replace(/_/g, "/"));
+
+export const toBase64Url = (base64: string) =>
   base64.replace(/\+/g, "-").replace(/\//g, "_");
 
-export const fromBase64Url = base64 =>
+export const fromBase64Url = (base64: string) =>
   base64.replace(/-/g, "+").replace(/_/g, "/");
 
-export default { encode };
+export const encode = (bytes: number[]) => {
+  const sourceText = toByteString(bytes);
+
+  return encodeString(sourceText);
+};
+
+export const decode = (text: string) => {
+  const decoded = decodeString(text);
+
+  return fromByteString(decoded);
+};
