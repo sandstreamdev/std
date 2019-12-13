2884561e8f322d8c4616875cb1a3bdefc80b17c2
Przemysław Zalewski <przemyslaw.zalewski@sandstream.pl>
2019-12-13 15:54:20 +0100

    Cover vector2 module

--- vector2/mul.ts
+++ vector2/mul.ts
@@ -1,4 +1,11 @@
-export default (matrix, point) => [
-  matrix.a * point[0] + matrix.c * point[1] + matrix.e,
-  matrix.b * point[0] + matrix.d * point[1] + matrix.f
-];
+export default (
+  {
+    a,
+    b,
+    c,
+    d,
+    e,
+    f
+  }: { a: number; c: number; e: number; b: number; d: number; f: number },
+  [x, y]: [number, number]
+) => [a * x + c * y + e, b * x + d * y + f];
--- vector2/scale.test.ts
+++ vector2/scale.test.ts
@@ -2,8 +2,48 @@
 // @ts-ignore ambiguous import
 import scale from "./scale.ts";
 
+// @ts-ignore ambiguous import
+import convertSpace from "./convertSpace.ts";
+
 describe("scale", () => {
-  it.skip("TODO", () => {
-    expect(scale()).toBeDefined();
+  it("creates a scale transform", () => {
+    expect(scale(2, 3)).toEqual({
+      a: 2,
+      b: 0,
+      c: 0,
+      d: 3,
+      e: 0,
+      f: 0
+    });
+  });
+
+  it("allows to scale a vector", () => {
+    const vector = [2, 3];
+    const space = scale(4, 5);
+
+    const [x, y] = convertSpace(space)(vector);
+
+    expect(x).toBeCloseTo(8, 5);
+    expect(y).toBeCloseTo(15, 5);
+  });
+
+  it("scales by 1 by default", () => {
+    const vector = [2, 3];
+    const space = scale();
+
+    const [x, y] = convertSpace(space)(vector);
+
+    expect(x).toBeCloseTo(2, 5);
+    expect(y).toBeCloseTo(3, 5);
+  });
+
+  it("scales uniformly by default", () => {
+    const vector = [2, 3];
+    const space = scale(3);
+
+    const [x, y] = convertSpace(space)(vector);
+
+    expect(x).toBeCloseTo(6, 5);
+    expect(y).toBeCloseTo(9, 5);
   });
 });
