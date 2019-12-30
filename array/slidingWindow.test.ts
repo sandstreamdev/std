2887081011e70b3a76eacc623d34b27b7db092b9
Krystian Boruciński <krystian.borucinski@sandstream.pl>
2019-12-30 15:07:08 +0100

    Add additional test cases

--- array/slidingWindow.test.ts
+++ array/slidingWindow.test.ts
@@ -1,90 +1,53 @@
 /* eslint-env jest */
 // @ts-ignore ambiguous import
-import { slidingWindow } from "./slidingWindow.ts";
+import slidingWindow from "./slidingWindow.ts";
 
 describe("slidingWindow", () => {
-  it("iterates by the 2-element window of values", () => {
-    const values = [1, 2, 3, 4, 5, 6];
-
-    const expected = [
+  it("should returns list, composed of 2-tuples of consecutive elements", () => {
+    expect(slidingWindow(2)([1, 2, 3, 4, 5, 6])).toEqual([
       [1, 2],
       [2, 3],
       [3, 4],
       [4, 5],
       [5, 6]
-    ];
-
-    const received = slidingWindow(2)(values);
-
-    expect(received).toEqual(expected);
+    ]);
   });
 
-  it("iterates by the 3-element window of values", () => {
-    const values = [1, 2, 3, 4, 5, 6];
-
-    const expected = [
+  it("should returns list, composed of 3-tuples of consecutive elements", () => {
+    expect(slidingWindow(3)([1, 2, 3, 4, 5, 6])).toEqual([
       [1, 2, 3],
       [2, 3, 4],
       [3, 4, 5],
       [4, 5, 6]
-    ];
-
-    const received = slidingWindow(3)(values);
-
-    expect(received).toEqual(expected);
+    ]);
   });
 
-  it("iterates by the 4-element window of values", () => {
-    const values = [1, 2, 3, 4, 5, 6];
-
-    const expected = [
+  it("should returns list, composed of 4-tuples of consecutive elements", () => {
+    expect(slidingWindow(4)([1, 2, 3, 4, 5, 6])).toEqual([
       [1, 2, 3, 4],
       [2, 3, 4, 5],
       [3, 4, 5, 6]
-    ];
-
-    const received = slidingWindow(4)(values);
-
-    expect(received).toEqual(expected);
+    ]);
   });
 
-  it("iterates by the 6-element window of values", () => {
-    const values = [1, 2, 3, 4, 5, 6];
-    const expected = [[1, 2, 3, 4, 5, 6]];
-
-    const received = slidingWindow(6)(values);
-
-    expect(received).toEqual(expected);
+  it("should returns wrapped array in array", () => {
+    expect(slidingWindow(6)([1, 2, 3, 4, 5, 6])).toEqual([[1, 2, 3, 4, 5, 6]]);
   });
 
-  it("iterates by the 5-element window of values", () => {
-    const values = [1, 2, 3, 4, 5, 6];
-
-    const expected = [
-      [1, 2, 3, 4, 5],
-      [2, 3, 4, 5, 6]
-    ];
-
-    const received = slidingWindow(5)(values);
-
-    expect(received).toEqual(expected);
+  it("should returns array of wrapped values in arrays", () => {
+    expect(slidingWindow(1)([1, 2, 3, 4, 5, 6])).toEqual([
+      [1],
+      [2],
+      [3],
+      [4],
+      [5],
+      [6]
+    ]);
   });
 
-  it("iterates by the one element window of values", () => {
-    const values = [1, 2, 3, 4, 5, 6];
-    const expected = [[1], [2], [3], [4], [5], [6]];
-
-    const received = slidingWindow(1)(values);
-
-    expect(received).toEqual(expected);
-  });
-
-  it("iterates empty array by the 2-element window of values", () => {
-    const values = [];
-    const expected = [];
-
-    const received = slidingWindow(3)(values);
-
-    expect(received).toEqual(expected);
+  it("should returns empty array", () => {
+    expect(slidingWindow(3)([])).toEqual([]);
+    expect(slidingWindow(0)([1, 2, 3])).toEqual([]);
+    expect(slidingWindow(-1)([1, 2, 3])).toEqual([]);
   });
 });
--- array/slidingWindow.ts
+++ array/slidingWindow.ts
@@ -1,4 +1,4 @@
 export default (count: number) => (xs: any[]) =>
   xs
     .map((_, index, source) => source.slice(index, index + count))
-    .filter(element => element.length === count);
+    .filter(element => element.length > 0 && element.length === count);
