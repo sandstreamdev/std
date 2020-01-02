68cf046b604c515a4a02372ec76f343bee41d515
Przemysław Zalewski <przemyslaw.zalewski@sandstream.pl>
2020-01-02 12:52:27 +0100

    Cover object/equals with tests

--- object/equals.test.ts
+++ object/equals.test.ts
@@ -3,7 +3,38 @@
 import equals from "./equals.ts";
 
 describe("equals", () => {
-  it.skip("TODO", () => {
-    expect(equals()).toBeDefined();
+  it("checks if two objects are deeply equal", () => {
+    const a = { 1: "test", b: { inner: true, other: false }, c: [1, 2, 3] };
+    const b = { 1: "test", b: { inner: true, other: false }, c: [1, 2, 3] };
+
+    const c = {
+      1: "test",
+      b: { inner: true, other: true, additional: 1 },
+      c: [1, 2, 3]
+    };
+
+    expect(equals(a, b)).toBe(true);
+    expect(equals(a, c)).toBe(false);
+  });
+
+  it("handles shallow equality", () => {
+    const a = { 1: "test", b: 3, c: [1, 2, 3] };
+    const b = { 1: "test", b: 3, c: [1, 2, 3] };
+    const c = { 1: "test", b: 3, c: [1, 2, 4] };
+
+    expect(equals(a, b)).toBe(true);
+    expect(equals(a, c)).toBe(false);
+  });
+
+  it("handles referential equality", () => {
+    const a = { 1: "test", b: { inner: true, other: false }, c: [1, 2, 3] };
+
+    expect(equals(a, a)).toBe(true);
+  });
+
+  it("supports arrays", () => {
+    expect(equals([1, 2, 3], [1, 2, 3])).toBe(true);
+    expect(equals([1, 2, 3], [1, 2, 3, 4, 5])).toBe(false);
+    expect(equals([1, 2, 3], [1, 8, 3])).toBe(false);
   });
 });
