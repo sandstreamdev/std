eb481158509381ec9a756e022ecce0c274cfbcca
Przemysław Zalewski <przemyslaw.zalewski@sandstream.pl>
2020-01-02 15:16:26 +0100

    Try to cover object/fromEntries and add tsconfig to allow native use of new features

--- object/fromEntries.test.ts
+++ object/fromEntries.test.ts
@@ -3,7 +3,37 @@
 import fromEntries from "./fromEntries.ts";
 
 describe("fromEntries", () => {
-  it.skip("TODO", () => {
-    expect(fromEntries()).toBeDefined();
+  it("creates an object from an array of entries", () => {
+    expect(
+      fromEntries([
+        ["a", 1],
+        ["b", 2],
+        ["c", 3]
+      ])
+    ).toEqual({ a: 1, b: 2, c: 3 });
+  });
+
+  it("overrides duplicated values", () => {
+    expect(
+      fromEntries([
+        ["a", 1],
+        ["b", 2],
+        ["c", 3],
+        ["c", 6],
+        ["a", 8]
+      ])
+    ).toEqual({ a: 8, b: 2, c: 6 });
+  });
+
+  it("handles empty entries", () => {
+    expect(fromEntries([])).toEqual({});
+  });
+
+  it("uses fallback implementation when Object.fromEntries is not present", () => {
+    if (Object.fromEntries) {
+      expect(fromEntries).toBe(Object.fromEntries);
+    } else {
+      expect(fromEntries).not.toBe(Object.fromEntries);
+    }
   });
 });
