ba49948f13393cc3c3cc9e11b95bedf2c4c19ce6
Przemysław Zalewski <przemyslaw.zalewski@sandstream.pl>
2020-01-02 15:50:43 +0100

    Cover object/sort

--- object/sort.test.ts
+++ object/sort.test.ts
@@ -3,7 +3,17 @@
 import sort from "./sort.ts";
 
 describe("sort", () => {
-  it.skip("TODO", () => {
-    expect(sort()).toBeDefined();
+  it("sorts the given object", () => {
+    const by = (a: number, b: number) => a - b;
+
+    expect(sort(by)({ a: 3, b: 2, c: 3, d: -7, e: 13, f: 0, g: 8 })).toEqual({
+      d: -7,
+      f: 0,
+      b: 2,
+      a: 3,
+      c: 3,
+      g: 8,
+      e: 13
+    });
   });
 });
