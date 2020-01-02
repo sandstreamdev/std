9c8cd08120a69f550480ced6d3d7b7fa3b9a8acc
Przemysław Zalewski <przemyslaw.zalewski@sandstream.pl>
2020-01-02 12:26:01 +0100

    Cover object/apply with tests

--- object/apply.test.ts
+++ object/apply.test.ts
@@ -3,7 +3,21 @@
 import apply from "./apply.ts";
 
 describe("apply", () => {
-  it.skip("TODO", () => {
-    expect(apply()).toBeDefined();
+  it("applies the given parameters to the given dictionary of functions", () => {
+    const lower = (text: string) => text.toLowerCase();
+    const upper = (text: string) => text.toUpperCase();
+
+    expect(apply({ lower, upper })("TeSt")).toEqual({
+      lower: "test",
+      upper: "TEST"
+    });
+  });
+
+  it("supports multiple arguments", () => {
+    const sum = (a: number, b: number) => a + b;
+    const max = (a: number, b: number) => Math.max(a, b);
+    const min = (a: number, b: number) => Math.min(a, b);
+
+    expect(apply({ sum, max, min })(5, 3)).toEqual({ sum: 8, max: 5, min: 3 });
   });
 });
