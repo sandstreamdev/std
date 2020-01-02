5d60765046adcba8581a513a53e87d8bb625f828
Przemysław Zalewski <przemyslaw.zalewski@sandstream.pl>
2020-01-02 13:33:53 +0100

    Cover object/first with tests and add typings

--- object/first.test.ts
+++ object/first.test.ts
@@ -3,7 +3,15 @@
 import first from "./first.ts";
 
 describe("first", () => {
-  it.skip("TODO", () => {
-    expect(first()).toBeDefined();
+  it("return the first value in the given object", () => {
+    expect(first({ a: 1, b: 2, c: 3 })).toBe(1);
+  });
+
+  it("return the first value in the given object", () => {
+    expect(first({ a: 1, 0: "test", 76: "test2", b: 2, c: 3 })).toBe("test");
+  });
+
+  it("handles empty objects", () => {
+    expect(first({})).toBe(undefined);
   });
 });
