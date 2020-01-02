de06bc90cdc419456c1c8bf3c84b6a0644a4f29c
Przemysław Zalewski <przemyslaw.zalewski@sandstream.pl>
2020-01-02 10:59:25 +0100

    Adjust typings of isomorphic base64url helpers

--- encoding/base64url.ts
+++ encoding/base64url.ts
@@ -1,6 +1,8 @@
 /* eslint-env browser, node */
 
-export const toByteString = bytes =>
+const toArray = (typedArray: Uint8Array): number[] => [...typedArray];
+
+export const toByteString = (bytes: number[]) =>
   bytes.map(_ => String.fromCharCode(_)).join("");
 
 export const fromByteString = (byteString: string) =>
@@ -10,15 +12,27 @@ const ENCODING = "utf-8";
 
 const btoaImplementation = (
   text: string,
-  context = typeof window !== "undefined" ? window : undefined
+  context: {
+    btoa: (byteString: string) => string;
+    TextEncoder: new () => {
+      encode: { (input?: string): Uint8Array };
+    };
+  } = typeof window !== "undefined" ? window : undefined
 ) =>
   context
-    ? context.btoa(toByteString([...new context.TextEncoder().encode(text)]))
+    ? context.btoa(
+        toByteString(toArray(new context.TextEncoder().encode(text)))
+      )
     : Buffer.from(text, ENCODING).toString("base64");
 
 const atobImplementation = (
   text: string,
-  context = typeof window !== "undefined" ? window : undefined
+  context: {
+    atob: (byteString: string) => string;
+    TextDecoder: new (encoding: string) => {
+      decode: (input?: Uint8Array) => string;
+    };
+  } = typeof window !== "undefined" ? window : undefined
 ) =>
   context
     ? new context.TextDecoder(ENCODING).decode(
@@ -26,14 +40,27 @@ const atobImplementation = (
       )
     : Buffer.from(text, "base64").toString(ENCODING);
 
-export const encode = (text: string, context) =>
+export const encode = (
+  text: string,
+  context?: {
+    btoa: (byteString: string) => string;
+    TextEncoder: new () => { encode: (input?: string) => Uint8Array };
+  }
+) =>
   btoaImplementation(text, context)
     .replace(/=/g, "")
     .replace(/\+/g, "-")
     .replace(/\//g, "_");
 
-export const decode = (text: string, context) =>
-  atobImplementation(text.replace(/-/g, "+").replace(/_/g, "/"), context);
+export const decode = (
+  text: string,
+  context?: {
+    atob: (byteString: string) => string;
+    TextDecoder: new (encoding: string) => {
+      decode: (input?: Uint8Array) => string;
+    };
+  }
+) => atobImplementation(text.replace(/-/g, "+").replace(/_/g, "/"), context);
 
 export const toBase64Url = (base64: string) =>
   base64.replace(/\+/g, "-").replace(/\//g, "_");
@@ -41,14 +68,28 @@ export const toBase64Url = (base64: string) =>
 export const fromBase64Url = (base64: string) =>
   base64.replace(/-/g, "+").replace(/_/g, "/");
 
-export const encodeBytes = (bytes: number[]) => {
+export const encodeBytes = (
+  bytes: number[],
+  context?: {
+    btoa: (byteString: string) => string;
+    TextEncoder: new () => { encode: (input?: string) => Uint8Array };
+  }
+) => {
   const sourceText = toByteString(bytes);
 
-  return encode(sourceText);
+  return encode(sourceText, context);
 };
 
-export const decodeBytes = (text: string) => {
-  const decoded = decode(text);
+export const decodeBytes = (
+  text: string,
+  context?: {
+    atob: (byteString: string) => string;
+    TextDecoder: new (encoding: string) => {
+      decode: (input?: Uint8Array) => string;
+    };
+  }
+) => {
+  const decoded = decode(text, context);
 
   return fromByteString(decoded);
 };
