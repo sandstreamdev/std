b5e2b77d2481d2546956301a442fac07d5ae3bb7
Przemysław Zalewski <przemyslaw.zalewski@sandstream.pl>
2020-01-02 12:40:42 +0100

    Cover object/enumerable with tests

--- object/enumerable.test.ts
+++ object/enumerable.test.ts
@@ -3,7 +3,30 @@
 import enumerable from "./enumerable.ts";
 
 describe("enumerable", () => {
-  it.skip("TODO", () => {
-    expect(enumerable()).toBeDefined();
+  it("creates a 1 to 1 mapping of given values as an object", () => {
+    expect(enumerable("TEST", "X", "Y")).toEqual({
+      TEST: "TEST",
+      X: "X",
+      Y: "Y"
+    });
+  });
+
+  it("overrides duplicated values", () => {
+    expect(enumerable("a", "b", "a")).toEqual({
+      a: "a",
+      b: "b"
+    });
+  });
+
+  it("is useful with object destructuring", () => {
+    const { SMALL, MEDIUM, BIG } = enumerable("SMALL", "MEDIUM", "BIG");
+
+    expect(SMALL).toEqual("SMALL");
+    expect(MEDIUM).toEqual("MEDIUM");
+    expect(BIG).toEqual("BIG");
+  });
+
+  it("handles no input gracefully", () => {
+    expect(enumerable()).toEqual({});
   });
 });
