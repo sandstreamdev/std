8614660291ad79271eca93016c3de71f870e6d7a
Przemysław Zalewski <przemyslaw.zalewski@sandstream.pl>
2020-01-03 15:12:06 +0100

    Rename test case to better match the intent and fix return/s typo

--- object/first.test.ts
+++ object/first.test.ts
@@ -3,11 +3,11 @@
 import first from "./first.ts";
 
 describe("first", () => {
-  it("return the first value in the given object", () => {
+  it("returns the first value in the given object", () => {
     expect(first({ a: 1, b: 2, c: 3 })).toBe(1);
   });
 
-  it("return the first value in the given object", () => {
+  it("follows default object iteration order", () => {
     expect(first({ a: 1, 0: "test", 76: "test2", b: 2, c: 3 })).toBe("test");
   });
 
