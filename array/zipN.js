3a2ebaee996bf46d9175812ea534283825ea0c5d
Krystian Boruciński <krystian.borucinski@sandstream.pl>
2020-01-09 15:44:17 +0100

    Update after code review

--- array/zipN.js
+++ array/zipN.js
@@ -1,5 +1,5 @@
 export default (...xs) => {
-  const [head, ...tail] = xs;
+  const [head = [], ...tail] = xs;
 
   return head.map((value, index) =>
     tail.reduce((x, xs) => [...x, xs[index]], [value])
