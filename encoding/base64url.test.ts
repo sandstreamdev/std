6582a42d57d729f38a8b0c20efe9d5cf87346160
Przemysław Zalewski <przemyslaw.zalewski@sandstream.pl>
2020-01-17 15:56:01 +0100

    Extend tests and descriptions of the module

--- encoding/base64url.test.ts
+++ encoding/base64url.test.ts
@@ -67,6 +67,14 @@ describe("base64url", () => {
     expect(encode(text)).toEqual("QmFzZTY0VVJMIGVuY29kZS9kZWNvZGUgdGVzdA");
   });
 
+  it("properly replaces non-URL-safe characters", () => {
+    expect(encode("<<???>>")).toEqual("PDw_Pz8-Pg");
+    expect(Buffer.from("<<???>>", "utf-8").toString("base64")).toEqual(
+      "PDw/Pz8+Pg=="
+    );
+    expect(decode(encode("<<???>>"))).toEqual("<<???>>");
+  });
+
   it("is does not include padding characters", () => {
     const text = "Zażółć gęślą jaźń";
 
@@ -93,6 +101,15 @@ describe("base64url", () => {
     expect(decode(encode(text))).toEqual(text);
   });
 
+  it("encodes/decodes bytes", () => {
+    const bytes = [0xc2, 0x67, 0xeb, 0xa7, 0x6d, 0x3e, 0x6c, 0x1b, 0x10, 0x7c];
+
+    const encoded = "w4Jnw6vCp20-bBsQfA";
+
+    expect(encodeBytes(bytes)).toEqual(encoded);
+    expect(decodeBytes(encoded)).toEqual(bytes);
+  });
+
   it("handles Unicode", () => {
     expect(decode(encode(unicodeText))).toEqual(unicodeText);
   });
@@ -111,9 +128,35 @@ describe("base64url", () => {
 
   it("converts base64URL to base64", () => {
     const text = toByteString(range(256));
+    const base64url = encode(text);
+    const encoded = toBase64(base64url);
+    const expected = Buffer.from(text, "utf-8").toString("base64");
 
-    expect(toBase64(encode(text))).toEqual(
-      Buffer.from(text, "utf-8").toString("base64")
-    );
+    expect(encoded).toEqual(expected);
+  });
+
+  it("fromBase64 strips padding characters", () => {
+    expect(fromBase64("PDw/Pz8+Pg==")).toEqual("PDw_Pz8-Pg");
+  });
+
+  it("toBase64 adds padding characters when needed", () => {
+    expect(toBase64("PDw_Pz8-Pg")).toEqual("PDw/Pz8+Pg==");
+  });
+
+  it("converts base64 to base64URL and vice versa", () => {
+    const base64 = "PDw/Pz8+Pg==";
+    const base64url = "PDw_Pz8-Pg";
+
+    expect(fromBase64(base64)).toEqual(base64url);
+    expect(toBase64(base64url)).toEqual(base64);
+  });
+
+  it("encodes/decodes bytes to and from string", () => {
+    const bytes = [0x50, 0x51, 0x52];
+
+    const encoded = "PQR";
+
+    expect(toByteString(bytes)).toEqual(encoded);
+    expect(fromByteString(encoded)).toEqual(bytes);
   });
 });
--- encoding/base64url/fromBase64.ts
+++ encoding/base64url/fromBase64.ts
@@ -1,2 +1,5 @@
 export default (base64: string) =>
-  base64.replace(/\+/g, "-").replace(/\//g, "_");
+  base64
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=/g, "");
