2791939f47158abcd96963c6775a8a1b2476be24
Przemysław Zalewski <przemyslaw.zalewski@sandstream.pl>
2020-02-05 13:17:06 +0100

    Split date/valid tests to check for Invalid Date in a separate test case

--- date/valid.test.ts
+++ date/valid.test.ts
@@ -3,9 +3,13 @@
 import valid from "./valid.ts";
 
 describe("valid", () => {
-  it("checks if the given date is a valid date", () => {
+  it("checks if the given date is valid", () => {
     expect(valid(new Date())).toBe(true);
     expect(valid(new Date("2020-01-31T09:52:31.618Z"))).toBe(true);
+  });
+
+  it("should reject Invalid Date objects", () => {
+    expect(valid(new Date("test"))).toBe(false);
     expect(valid(new Date("77724e10-2ffa-45cc-b05d-a91a846a6080"))).toBe(false);
   });
 
