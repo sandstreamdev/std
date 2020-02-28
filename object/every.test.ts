8dd3143fef89a19f17736fcec86c9ce86cedc727
Przemysław Zalewski <przemyslaw.zalewski@sandstream.pl>
2020-02-28 19:16:40 +0100

    Add object/{some,every} implementation

---
+++ object/every.test.ts
@@ -0,0 +1,27 @@
+/* eslint-env jest */
+// @ts-ignore ambiguous import
+import every from "./every.ts";
+
+describe("every", () => {
+  it("test if every element passes the given predicate", () => {
+    const adult = ({ age }: { age: number }) => age >= 18;
+
+    expect(
+      every(adult)({
+        tom: { age: 25 },
+        john: { age: 16 },
+        alice: { age: 18 }
+      })
+    ).toBe(false);
+  });
+
+  it("test if every element passes the given predicate", () => {
+    expect(
+      every((x: number) => x >= 0)({
+        x: 5,
+        y: 3,
+        z: 0
+      })
+    ).toBe(true);
+  });
+});
