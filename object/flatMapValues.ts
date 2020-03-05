ba439b484c80fb508646220df42842982d4a03cd
Przemysław Zalewski <przemyslaw.zalewski@sandstream.pl>
2020-03-05 11:57:57 +0100

    Inspect object module and fill docs

--- object/flatMapValues.ts
+++ object/flatMapValues.ts
@@ -1,6 +1,6 @@
 import flatMap from "../array/flatMap";
 import entries from "./entries";
 
-export default (f: (value: any, key: string, context: object) => boolean) => (
+export default (f: (value: any, key: string, context: object) => any) => (
   xs: object
 ): any[] => flatMap(([key, value]) => f(value, key, xs))(entries(xs));
