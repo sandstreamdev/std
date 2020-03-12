1a32f8ea16fc07ac277eecaa97229045a7458d19
Przemysław Zalewski <przemyslaw.zalewski@sandstream.pl>
2020-03-12 15:05:41 +0100

    Cover date/fromDays with tests and update function docs

--- date/fromDays.test.ts
+++ date/fromDays.test.ts
@@ -3,7 +3,15 @@
 import fromDays from "./fromDays.ts";
 
 describe("fromDays", () => {
-  it.skip("TODO", () => {
-    expect(fromDays()).toBeDefined();
+  it("converts given day count to milliseconds", () => {
+    expect(fromDays(1)).toEqual(86400000);
+  });
+
+  it("should handle sub-day resolution", () => {
+    expect(fromDays(0.5)).toEqual(43200000);
+  });
+
+  it("should handle over 24h resolution", () => {
+    expect(fromDays(2.5)).toEqual(216000000);
   });
 });
