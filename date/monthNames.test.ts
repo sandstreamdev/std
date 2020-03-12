3fe2e84ab4c7f9e000835ed522f66db25dcf57d4
Przemysław Zalewski <przemyslaw.zalewski@sandstream.pl>
2020-03-12 14:00:55 +0100

    Remove locale specific month names and month formatting function

--- date/monthNames.test.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-env jest */
-// @ts-ignore ambiguous import
-import monthNames from "./monthNames.ts";
-
-describe("monthNames", () => {
-  it.skip("TODO", () => {
-    expect(monthNames()).toBeDefined();
-  });
-});
