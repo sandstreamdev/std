7624a118f1774164c86728b109e5ac0e5b6443f1
Przemysław Zalewski <przemyslaw.zalewski@sandstream.pl>
2020-03-12 14:21:08 +0100

    Cover date/displayTime with tests and update function docs

--- date/displayTime.test.ts
+++ date/displayTime.test.ts
@@ -3,7 +3,11 @@
 import displayTime from "./displayTime.ts";
 
 describe("displayTime", () => {
-  it.skip("TODO", () => {
-    expect(displayTime()).toBeDefined();
+  it("displays padded time string", () => {
+    expect(displayTime([5, 12, 16], false)).toBe("05:12");
+  });
+
+  it("includes seconds display if enabled", () => {
+    expect(displayTime([5, 12, 16], true)).toBe("05:12:16");
   });
 });
