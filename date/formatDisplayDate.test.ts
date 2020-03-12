ab755b22a2c12bfa4a6c067c1a8a30d9d3a9df6e
Przemysław Zalewski <przemyslaw.zalewski@sandstream.pl>
2020-03-12 14:12:50 +0100

    Remove non locale aware formatDisplayDate

--- date/formatDisplayDate.test.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-env jest */
-// @ts-ignore ambiguous import
-import formatDisplayDate from "./formatDisplayDate.ts";
-
-describe("formatDisplayDate", () => {
-  it.skip("TODO", () => {
-    expect(formatDisplayDate()).toBeDefined();
-  });
-});
