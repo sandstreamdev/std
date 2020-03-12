4f3d351e3bba9e6dd33fb893313170240e215fb7
Przemysław Zalewski <przemyslaw.zalewski@sandstream.pl>
2020-03-12 13:44:42 +0100

    Cover date/daysInYear with tests and update function docs

--- date/daysInYear.test.ts
+++ date/daysInYear.test.ts
@@ -3,7 +3,19 @@
 import daysInYear from "./daysInYear.ts";
 
 describe("daysInYear", () => {
-  it.skip("TODO", () => {
-    expect(daysInYear()).toBeDefined();
+  it("should handle common years", () => {
+    const years = [2017, 2018, 2019, 2022, 2023];
+
+    years.forEach(year => {
+      expect(daysInYear(year)).toBe(365);
+    });
+  });
+
+  it("should handle leap years", () => {
+    const years = [2016, 2020, 2048];
+
+    years.forEach(year => {
+      expect(daysInYear(year)).toBe(366);
+    });
   });
 });
