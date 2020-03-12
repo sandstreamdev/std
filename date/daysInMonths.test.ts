4f32fdda57f8b9fdc443dc8664cdacd13068806b
Przemysław Zalewski <przemyslaw.zalewski@sandstream.pl>
2020-03-12 13:40:52 +0100

    Cover date/daysInMonths with tests and update function docs

--- date/daysInMonths.test.ts
+++ date/daysInMonths.test.ts
@@ -3,7 +3,41 @@
 import daysInMonths from "./daysInMonths.ts";
 
 describe("daysInMonths", () => {
-  it.skip("TODO", () => {
-    expect(daysInMonths()).toBeDefined();
+  it("should handle common years", () => {
+    const commonYearMonthDays = daysInMonths(false);
+
+    expect(commonYearMonthDays).toEqual([
+      31,
+      28,
+      31,
+      30,
+      31,
+      30,
+      31,
+      31,
+      30,
+      31,
+      30,
+      31
+    ]);
+  });
+
+  it("should handle leap years", () => {
+    const leapYearMonthDays = daysInMonths(true);
+
+    expect(leapYearMonthDays).toEqual([
+      31,
+      29,
+      31,
+      30,
+      31,
+      30,
+      31,
+      31,
+      30,
+      31,
+      30,
+      31
+    ]);
   });
 });
