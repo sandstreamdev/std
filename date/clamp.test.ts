16c80e1aaafb4dcb43ace3c0f4f2975dec0a2ecf
Przemysław Zalewski <przemyslaw.zalewski@sandstream.pl>
2020-03-12 09:02:58 +0100

    Cover date/clamp with tests and update function docs

--- date/clamp.test.ts
+++ date/clamp.test.ts
@@ -3,7 +3,36 @@
 import clamp from "./clamp.ts";
 
 describe("clamp", () => {
-  it.skip("TODO", () => {
-    expect(clamp()).toBeDefined();
+  it("should do nothing when value fits the range", () => {
+    const date = new Date("2019-02-28T13:54:33.232Z");
+    const min = new Date("2019-02-23T13:54:33.232Z");
+    const max = new Date("2019-03-13T13:54:33.232Z");
+
+    const clampToRange = clamp(min, max);
+    const clamped = clampToRange(date);
+
+    expect(clamped).toEqual(date);
+  });
+
+  it("should clamp to upper bound", () => {
+    const date = new Date("2019-06-15T13:54:33.232Z");
+    const min = new Date("2019-02-23T13:54:33.232Z");
+    const max = new Date("2019-03-13T13:54:33.232Z");
+
+    const clampToRange = clamp(min, max);
+    const clamped = clampToRange(date);
+
+    expect(clamped).toEqual(max);
+  });
+
+  it("should clamp to lower bound", () => {
+    const date = new Date("2019-01-15T13:54:33.232Z");
+    const min = new Date("2019-02-23T13:54:33.232Z");
+    const max = new Date("2019-03-13T13:54:33.232Z");
+
+    const clampToRange = clamp(min, max);
+    const clamped = clampToRange(date);
+
+    expect(clamped).toEqual(min);
   });
 });
