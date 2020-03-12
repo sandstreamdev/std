b8cc0fe17b87befc1a74bedbde4b7085d7cde274
Przemysław Zalewski <przemyslaw.zalewski@sandstream.pl>
2020-03-12 15:58:39 +0100

    Cover date/leapYear with tests and update function docs

--- date/leapYear.test.ts
+++ date/leapYear.test.ts
@@ -3,7 +3,19 @@
 import leapYear from "./leapYear.ts";
 
 describe("leapYear", () => {
-  it.skip("TODO", () => {
-    expect(leapYear()).toBeDefined();
+  it("should detect leap years", () => {
+    const years = [2016, 2020, 2048];
+
+    years.forEach(year => {
+      expect(leapYear(year)).toBe(true);
+    });
+  });
+
+  it("should not detect common years", () => {
+    const years = [2017, 2018, 2019, 2022, 2023];
+
+    years.forEach(year => {
+      expect(leapYear(year)).toBe(false);
+    });
   });
 });
