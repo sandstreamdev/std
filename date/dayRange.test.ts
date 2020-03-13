62561d4c5a2c4f3d5034aa99db97baa43ffc921e
Przemysław Zalewski <przemyslaw.zalewski@sandstream.pl>
2020-03-13 13:12:34 +0100

    Cover date/dayRange with tests and update function docs

--- date/dayRange.test.ts
+++ date/dayRange.test.ts
@@ -1,9 +1,15 @@
 /* eslint-env jest */
 // @ts-ignore ambiguous import
 import dayRange from "./dayRange.ts";
+// @ts-ignore ambiguous import
+import startOfDay from "./startOfDay.ts";
+// @ts-ignore ambiguous import
+import endOfDay from "./endOfDay.ts";
 
 describe("dayRange", () => {
-  it.skip("TODO", () => {
-    expect(dayRange()).toBeDefined();
+  it("returns a local day range at a particular Date", () => {
+    const date = new Date("2018-12-31T13:54:33.232Z");
+
+    expect(dayRange(date)).toEqual([startOfDay(date), endOfDay(date)]);
   });
 });
--- date/offsetByBit.test.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-env jest */
-// @ts-ignore ambiguous import
-import offsetByBit from "./offsetByBit.ts";
-
-describe("offsetByBit", () => {
-  it.skip("TODO", () => {
-    expect(offsetByBit()).toBeDefined();
-  });
-});
