2a695c1a6c648eb39de13145c6934eec66a1bb9f
Przemysław Zalewski <przemyslaw.zalewski@sandstream.pl>
2020-03-13 14:38:40 +0100

    Cover date/format* with tests and update function docs

--- date/formatTime.test.ts
+++ date/formatTime.test.ts
@@ -3,7 +3,33 @@
 import formatTime from "./formatTime.ts";
 
 describe("formatTime", () => {
-  it.skip("TODO", () => {
-    expect(formatTime()).toBeDefined();
+  it("formats a given date as a simple HH:MM(:SS) string", () => {
+    const a = new Date("2019-02-15T12:00:00");
+    const b = new Date("2019-02-23T23:30:00");
+    const c = new Date("2019-02-24T01:12:34");
+
+    expect(formatTime(a)).toEqual("12:00");
+    expect(formatTime(b)).toEqual("23:30");
+    expect(formatTime(c)).toEqual("01:12");
+  });
+
+  it("includes seconds display when enabled", () => {
+    const a = new Date("2019-02-15T12:00:00");
+    const b = new Date("2019-02-23T23:30:00");
+    const c = new Date("2019-02-24T01:12:34");
+
+    expect(formatTime(a, true)).toEqual("12:00:00");
+    expect(formatTime(b, true)).toEqual("23:30:00");
+    expect(formatTime(c, true)).toEqual("01:12:34");
+  });
+
+  it("does not include seconds display by default", () => {
+    const a = new Date("2019-02-15T12:00:00");
+    const b = new Date("2019-02-23T23:30:00");
+    const c = new Date("2019-02-24T01:12:34");
+
+    expect(formatTime(a)).toEqual(formatTime(a, false));
+    expect(formatTime(b)).toEqual(formatTime(b, false));
+    expect(formatTime(c)).toEqual(formatTime(c, false));
   });
 });
